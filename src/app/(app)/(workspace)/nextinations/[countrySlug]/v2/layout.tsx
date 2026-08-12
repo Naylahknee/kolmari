c8bae8bdea84460452ed3475294b0fd3751858c8
Naylahknee <95973671+Naylahknee@users.noreply.github.com>
2026-08-12 15:38:26 -0700

    Load refreshed country overview styles

--- src/app/(app)/(workspace)/nextinations/[countrySlug]/v2/layout.tsx
+++ src/app/(app)/(workspace)/nextinations/[countrySlug]/v2/layout.tsx
@@ -1,4 +1,5 @@
 import '@/styles/country-template.css'
+import '@/styles/country-overview-refresh.css'
 
 export default function V2Layout({ children }: { children: React.ReactNode }) {
   return <>{children}</>
