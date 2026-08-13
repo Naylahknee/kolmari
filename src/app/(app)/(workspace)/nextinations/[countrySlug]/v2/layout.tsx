d2c223d484cc40da134e0a7b10f90fdd474c30c3
Naylahknee <95973671+Naylahknee@users.noreply.github.com>
2026-08-12 18:39:08 -0700

    Load country overview structural corrections last

--- src/app/(app)/(workspace)/nextinations/[countrySlug]/v2/layout.tsx
+++ src/app/(app)/(workspace)/nextinations/[countrySlug]/v2/layout.tsx
@@ -1,6 +1,7 @@
 import '@/styles/country-template.css'
 import '@/styles/country-overview-refresh.css'
 import '@/styles/country-overview-read.css'
+import '@/styles/country-overview-layout-fix.css'
 
 export default function V2Layout({ children }: { children: React.ReactNode }) {
   return <>{children}</>
